a35804f6b89128cfa2d8027020615b30adbe7aa3
Mulirow <mbn2@cin.ufpe.br>
2021-12-05 15:35:31 -0300

    Finished Q9.c
    Co-authored-by: RenatoNogFer <RenatoNogFer@users.noreply.github.com>
    Co-authored-by: Kammerer; <victorKammerer@users.noreply.github.com>
    Co-authored-by: Matheus J. Boncsidai <Mathbonc@users.noreply.github.com>

--- Projeto/q9/Q9.c
+++ Projeto/q9/Q9.c
@@ -2,46 +2,79 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int *array, contador = 0;
-int N, T;
+int *array,*guardadordeprimos, *kc;
+int N, T, prime = 0;
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *Erastosthenes(void *threadid);
+void *Eratosthenes(void *threadid){
+    int ID = (*(int *)threadid);
+    int i;
+
+    while(prime * prime <= N){
+        //Escolhendo o proximo elemento
+        pthread_mutex_lock(&mymutex);
+        for(i = prime + 1; i <= N; i++){
+            if(array[i] == 1){
+                prime = i;
+                break;
+            }
+        }
+        guardadordeprimos[ID]=prime;
+        pthread_mutex_unlock(&mymutex);
+
+        printf("%d encontrado pela thread %d\n", prime, ID);
+
+        //Eliminação de não-primos
+        for(kc[ID] = guardadordeprimos[ID] + guardadordeprimos[ID]; kc[ID] < N; kc[ID] += guardadordeprimos[ID]){
+            array[kc[ID]] = 0;
+        }
+    }
+
+    pthread_exit(NULL);
+}
 
 int main(){
     int i;
-    
+
     scanf("%d %d", &N, &T);
     pthread_t Threads[T];
     int ThreadsID[T], rc;
     array = (int *) malloc(N * sizeof(int));
+    guardadordeprimos = (int *) malloc(T * sizeof(int));
+    kc = (int *) malloc(T * sizeof(int));
 
     for(i = 0; i < N; i++){
-        if(i == 0 || i == 1){
-            array[i] = 0;
-        }
-        else{
-            array[i] = 1;
+        array[i] = 1;
+    }
+
+    array[0] = 0;
+    array[1] = 0;
+
+    for(i = 0; i < T; i++){
+        ThreadsID[i] = i;
+        rc = pthread_create(&Threads[i], NULL, Eratosthenes, &ThreadsID[i]);
+        printf("Criando Thread: %d\n", i);
+        if(rc){
+            printf("Falhou na criação da Thread %d\n", i);
+            exit(1);
         }
     }
 
     for(i = 0; i < T; i++){
-        ThreadsID[i] = i;
-        rc = pthread_create(&Threads[i], NULL, Erastosthenes, &ThreadsID[i]);
-        printf("Criando Thread: %d\n", i);
-        if(rc){
-            printf("Falhou na criação da Thread %d\n", i);
-            exit(-1);
-        }
+        pthread_join(Threads[i], NULL);
     }
 
+    int count = 0;
+    printf("Primos encontrados: ");
+    for(i = 0; i < N; i++){
+        if(array[i] == 1){
+            count++;
+        }
+    }
+    printf("%d\n", count);
+
     free(array);
+    free(guardadordeprimos);
+    free(kc);
     pthread_exit(NULL);
 }
-
-//Proibido ser feliz
-//Tem questão 9?
-// ss
-// onde???
-// dps da 8
-// '-' vi agora
-// :)
